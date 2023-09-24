f8997a5b8edb0aaf8607c9f02f88b5dcab398140
unknown <falgunidable@gmail.com>
2023-09-24 17:14:31 +0530

    operators file created

---
+++ opeators.py
@@ -0,0 +1,12 @@
+op = 40
+
+print('')
+
+#if else statements
+# if op > 10:
+#     print("greater")
+# else:
+#     print("not")
+#ternary operator
+print("greater") if op > 10 else print("not")
+print('')
---
+++ welcome.py
@@ -0,0 +1,10 @@
+line1 = "***********"
+line2 = "*         *" #reuse
+line3 = "* WELCOME *"
+
+print('')
+print(line1)
+print(line2)
+print(line3)
+print(line2)
+print(line1)
