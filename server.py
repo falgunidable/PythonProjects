a1dffa03d883cbb9fbf52abe00291694ef4353d6
unknown <falgunidable@gmail.com>
2023-10-13 13:56:20 +0530

    created a web page using html & css and connected it with python using flask for displaying the web pages

---
+++ server.py
@@ -0,0 +1,37 @@
+#waitress - helps to serve the app during production
+from flask import Flask, render_template, request
+from weather import get_current_weather
+from waitress import serve
+
+app = Flask(__name__)   # this makes it the flask app
+
+# routes that are accessed on the web
+@app.route('/')
+@app.route('/index')
+def index():        # function to return something for the routes
+    # return 'Hello World !'
+    return render_template('index.html')
+
+@app.route('/weather')
+def get_weather():
+    city = request.args.get('city')
+
+    # check for empty strings or strings with spaces
+    if not bool(city.strip()):
+        city = 'Pune'
+
+    weather_data = get_current_weather(city)
+
+    # city is not found by the api
+    # if not weather_data['current']['condition']['code'] == 200:
+    #     return render_template('city_not_found.html')
+
+    return render_template(
+        "weather.html",
+        title = weather_data['location']['name'],
+        feels_like = f"{weather_data['current']['feelslike_c']:.1f}",
+        calc_on = weather_data['location']['localtime']
+    )
+
+if __name__ == '__main__':
+    serve(app , host="0.0.0.0", port=8000)
--- weather.py
+++ weather.py
@@ -4,24 +4,32 @@ from dotenv import load_dotenv
 import os
 from pprint import pprint
 
-load_dotenv()
+load_dotenv()       # imports the api key from th env file
 
-def get_current_weather():
-    print("\n*** Get Current Weather Conditions ***\n")
-
-    city = input("\nPlease enter a city name:\n")
-
+def get_current_weather(city='Pune'):
 
     request_url = f'http://api.weatherapi.com/v1/current.json?key={os.getenv("API_KEY")}&q={city}'
-
-    print(request_url)
-
+    
     weather_data = requests.get(request_url).json()
+
+    return weather_data
+    # print(request_url)
+
     # pprint(weather_data)
 
-    print(f"\nCurrent weather for {weather_data['location']['name']}\n")
-    print(f"Feels Like {weather_data['current']['feelslike_c']}\n")
-    print(f"Temperature calulated around {weather_data['location']['localtime']}\n")
+    # print(f"\nCurrent weather for {weather_data['location']['name']}\n")
+    # print(f"Feels Like {weather_data['current']['feelslike_c']}\n")
+    # print(f"Temperature calulated around {weather_data['location']['localtime']}\n")
 
 if __name__ == '__main__':
-    get_current_weather()
+
+    print("\n*** Get Current Weather Conditions ***\n")
+    city = input("\nPlease enter a city name:\n")
+
+    # check for empty strings or strings with spaces
+    if not bool(city.strip()):
+        city = 'Pune'
+
+    weather = get_current_weather(city)
+    
+    pprint(weather)
