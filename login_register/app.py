614be73c59e9f5b804fd15b9108e276f54ff478a
unknown <falgunidable@gmail.com>
2023-10-16 16:20:18 +0530

    registration of user done successfully

--- login_register/app.py
+++ login_register/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
 from flask_mysqldb import MySQL
+import MySQLdb.cursors as c
 from waitress import serve
 
 app = Flask(__name__ , static_folder='static')
@@ -17,9 +18,30 @@ def login():        # function to return something for the routes
     # return 'Hello World !'
     return render_template('login.html')
 
-@app.route('/register')
+@app.route('/register', methods = ['GET','POST'])
 def register():
-    return render_template('register.html')
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+
+        cursor = mysql.connection.cursor(c.DictCursor)
+        cursor.execute("SELECT * FROM accounts where username = % s and password = % s" , (username , password))
+        account = cursor.fetchone()
+
+        if account:
+            msg = 'Account Already Exists !'
+        elif not username or not password or not email:
+            msg = 'Please fill out the fields...'
+        else:
+            cursor.execute('INSERT INTO accounts VALUES (NULL, % s, % s, % s)', (username, password, email))
+            mysql.connection.commit()
+            msg = 'You have successfully registered !'
+    elif request.method == 'POST':
+            msg = 'Please fill out the form !'
+
+    return render_template('register.html', msg = msg)
 
 if __name__ == '__main__':
     serve(app , host="0.0.0.0", port=8000)
