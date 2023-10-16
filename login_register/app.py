8a493e6a9b403a56be7c88c3ee5ed63e1414170a
unknown <falgunidable@gmail.com>
2023-10-16 16:44:56 +0530

    login made successful with db credentials and an index page

--- login_register/app.py
+++ login_register/app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors as c
+import os
 from waitress import serve
 
 app = Flask(__name__ , static_folder='static')
+
+secret_key = os.urandom(24).hex()
+app.secret_key = secret_key
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
@@ -11,12 +15,6 @@ app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'signinsignup'
 
 mysql = MySQL(app)
-
-@app.route('/')
-@app.route('/login')
-def login():        # function to return something for the routes
-    # return 'Hello World !'
-    return render_template('login.html')
 
 @app.route('/register', methods = ['GET','POST'])
 def register():
@@ -42,6 +40,33 @@ def register():
             msg = 'Please fill out the form !'
 
     return render_template('register.html', msg = msg)
+
+@app.route('/')
+@app.route('/login', methods = ['GET','POST'])
+def login():        
+    msg =''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(c.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = % s AND password = % s', (username, password))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            msg = 'Login Successfull !'
+            return render_template('index.html', msg = msg)
+        else:
+            msg = 'Incorrect username / password !'
+    return render_template('login.html', msg = msg)
+
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    return render_template('login.html')
 
 if __name__ == '__main__':
     serve(app , host="0.0.0.0", port=8000)
